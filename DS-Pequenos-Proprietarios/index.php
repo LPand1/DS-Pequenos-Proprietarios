8a3e3624ba2b0c1f9402115ce4a0372b634b641c
Lorenzo Andreolli Pilotti <l23pilotti@gmail.com>
2026-06-10 08:15:21 -0300

    Add files via upload

---
+++ DS-Pequenos-Proprietarios/index.php
@@ -0,0 +1,3 @@
+<?php
+require_once __DIR__ . '/config/autoload.php';
+require_once __DIR__ . '/routes/rotas.php';
---
+++ DS-Pequenos-Proprietarios/models/class.Usuario.php
@@ -0,0 +1,22 @@
+<?php
+Class Usuario implements jsonSerializable {
+    private $id;
+    private $senha;
+    private $cpf;
+
+    public function setId($id) { $this->id = $id; } 
+    public function getId() { return $this->id; }
+    public function setSenha($s) { $this->senha = $s; }
+    public function getSenha() { return $this->senha; }
+    public function setCpf($c) { $this->cpf = $c; }
+    public function getCpf() { return $this->cpf; }
+
+    public function jsonSerialize() : mixed {
+        return  [
+            'id' => $this->id,
+            'cpf' => $this->cpf,
+        ];
+    }
+}
+
+?>
